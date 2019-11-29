99f2263b239b67e0a9cf670efe6e073c0be1c61f
Hlinovets <bary_set@mail.ru>
2019-11-29 21:14:12 +0300

    29sl

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const app = express();
+ // определяем Router
+const productRouter = express.Router();
+productRouter.use("/create", function(request, response){
+  response.send("Добавление товара"); });
+productRouter.use("/:id", function(request, response){
+  response.send(`Товар ${request.params.id}`);});
+productRouter.use("/", function(request, response){
+  response.send("Список товаров");});
+app.use("/products", productRouter);
  app.use("/about", function (request, response) {
-  response.send("О сайте");
-});
- app.use("/products/create",function (request, response) {
-  response.send("Добавление товара");
-});
-app.use("/products/:id",function (request, response) {
-  response.send(`Товар ${request.params.id}`);
-});
-app.use("/products/",function (request, response) {
-  response.send("Список товаров");
-});
+  response.send("О сайте");});
  app.use("/", function (request, response) {
-  response.send("Главная страница");
-});
+  response.send("Главная страница");});
 app.listen(3000);
