0383f1e8e6b268c16aa3fa5b6e422fcabcf766eb
Hlinovets <bary_set@mail.ru>
2019-11-29 20:00:25 +0300

    3sl

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const http = require("http");
-http.createServer(function(request, response){
-      response.end("Hello world!");
-    console.log("Url: " + request.url);
-    console.log("Тип запроса: " + request.method);
-    console.log("User-Agent: " + request.headers["user-agent"]);
-    console.log("Все заголовки");
-    console.log(request.headers);
-    response.end();
-}).listen(3000);  
+ http.createServer(function(request, response){
+     response.setHeader("UserId", 12);
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+    response.write("<h2>hello world</h2>");
+    response.end();
+}).listen(3000);
