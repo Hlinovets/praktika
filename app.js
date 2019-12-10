a502015f6e303bbab3f4b69f2fbe4c32d411494d
Hlinovets <56333650+Hlinovets@users.noreply.github.com>
2019-12-10 11:06:03 +0400

    2sql

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const mysql = require("mysql2");
  const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  database: "test",
-  password: "usbw"
+database: "test",
+  password: "usbw",
+port: "3307"
 });
+connection.connect(function(err){
+        if (err) {
+          return console.error("Ошибка: " + err.message);    }
+        else{
+          console.log("Подключение к серверу MySQL успешно установлено");    }
+});
+    
