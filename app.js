c3643207b414b82c223c32d32d0ee9097573606c
Hlinovets <56333650+Hlinovets@users.noreply.github.com>
2019-12-12 17:39:02 +0400

    11sql

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ database: "usersdb2",
   password: "usbw",
 port: "3307"
 });
- const sql = "INSERT INTO users(name, age) VALUES('Sam', 31)";
- connection.query(sql, function(err, results) {
+ const users = [
+      ["Bob", 22],
+      ["Alice", 25],
+      ["Kate", 28]
+    ];
+    
+ const sql = "INSERT INTO users(name, age) VALUES ?";
+ connection.query(sql, [users], function(err, results) {
     if(err) console.log(err);
     console.log(results);
 });
